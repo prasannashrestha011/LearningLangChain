f19df9d9e6cbbe9a00a7feeff53890f7a00b2905
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-10-25 21:04:35 +0545

    feat: learned and created a deterministic and autonomous function

--- custom_model/sql_agent/main.py
+++ custom_model/sql_agent/main.py
@@ -1,52 +1,27 @@
 from langchain.chat_models import init_chat_model
+from langchain.tools.tool_node import _ToolNode
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from dotenv import load_dotenv
-from langchain.agents import create_agent
+from langchain_core.messages import AIMessage
 load_dotenv()
 db=SQLDatabase.from_uri("sqlite:///Chinook.db")
 llm=init_chat_model(model="gemini-2.5-flash",model_provider="google_genai")
 toolkit=SQLDatabaseToolkit(db=db,llm=llm)
 tools=toolkit.get_tools()
 
-system_prompt = """
-You are an agent designed to interact with a SQL database.
-Given an input question, create a syntactically correct {dialect} query to run,
-then look at the results of the query and return the answer. Unless the user
-specifies a specific number of examples they wish to obtain, always limit your
-query to at most {top_k} results.
+get_schema_tool=next(tool for tool in tools if tool.name=="sql_db_schema")
+get_schema_node=_ToolNode([get_schema_tool],name="get_schema")
 
-You can order the results by a relevant column to return the most interesting
-examples in the database. Never query for all the columns from a specific table,
-only ask for the relevant columns given the question.
+run_query_tool=next(tool for tool in tools if tool.name=="sql_db_query")
+run_schema_node=_ToolNode([run_query_tool],name="run_query")
 
-You MUST double check your query before executing it. If you get an error while
-executing a query, rewrite the query and try again.
-
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the
-database.
-
-To start you should ALWAYS look at the tables in the database to see what you
-can query. Do NOT skip this step.
-
-Then you should query the schema of the most relevant tables.
-""".format(
-    dialect=db.dialect,
-    top_k=5,
-)
-
-agent=create_agent(
-        llm,
-        tools,
-        system_prompt=system_prompt
-        )
-
-while True:
-    question =input("H: ")
-    if question.lower()== "exit":
-        break
-    for step in agent.stream(
-        {"messages": [{"role": "user", "content": question}]},
-            stream_mode="values",
-        ):
-            step["messages"][-1].pretty_print()
+tool_call={
+        "name":"sql_db_list_tables",
+        "args":{},
+        "id":"abc123",
+        "type":"tool_call"
+    }
+tool_call_message=AIMessage(content="",tool_call=[tool_call])
+list_table_tool=next(tool for tool in tools if tool.name=="sql_db_list_tables")
+print(list_table_tool.invoke(tool_call))
